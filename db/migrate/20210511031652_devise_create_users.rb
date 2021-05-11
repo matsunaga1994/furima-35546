4ec2a4ce5c0a58009e920d9802a9ebbacc88d228
matsunaga1994 <82854197+matsunaga1994@users.noreply.github.com>
2021-05-11 15:21:11 +0900

    新規登録機能実装

--- db/migrate/20210511031652_devise_create_users.rb
+++ db/migrate/20210511031652_devise_create_users.rb
@@ -10,7 +10,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :family_name,        null: false
       t.string :first_name,         null: false
       t.string :family_name_kana,   null: false
-      t.string :first_name_kaba,    null: false
+      t.string :first_name_kana,    null: false
       t.date   :birthday,           null: false
 
       ## Recoverable
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_05_11_031652) do
     t.string "family_name", null: false
     t.string "first_name", null: false
     t.string "family_name_kana", null: false
-    t.string "first_name_kaba", null: false
+    t.string "first_name_kana", null: false
     t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
