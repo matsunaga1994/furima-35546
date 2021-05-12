aecd4f04c61646d83e2a8ecafa968dc5986d36d5
matsunaga1994 <82854197+matsunaga1994@users.noreply.github.com>
2021-05-12 14:48:37 +0900

    バリデーションのリファクタリング（その２））

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :family_name
+    validates :first_name
+    validates :family_name_kana
+    validates :first_name_kana
+    validates :birthday
+  end
+
 
   with_options format:{ with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}\z/i } do
     validates :password
