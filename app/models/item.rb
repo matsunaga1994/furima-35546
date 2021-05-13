2fd01d9444e5a24d4f57c2f5c769132634b9ee55
matsunaga1994 <82854197+matsunaga1994@users.noreply.github.com>
2021-05-13 12:03:23 +0900

    モデル作成、アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,31 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :burden
+  belongs_to :day
+  belongs_to :prefecture
+  belongs_to :status
+
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :category_id
+    validates :status_id
+    validates :burden_id
+    validates :prefecture_id
+    validates :day_id
+    validates :price
+    validates :image
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :burden_id
+    validates :prefecture_id
+    validates :day_id
+  end
 end
