a8740816b921ce02bc407b7b12fa1e7ed1a5bec1
matsunaga1994 <82854197+matsunaga1994@users.noreply.github.com>
2021-05-14 17:55:46 +0900

    before_actionの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :destroy]
+  before_action :remove_to_index, only: :destroy
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -24,14 +25,19 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    if current_user.id == item.user_id
-      item.destroy
-    end
+    item.destroy
     redirect_to root_path
   end
 
   private
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :status_id, :burden_id, :prefecture_id, :day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def remove_to_index
+    item = Item.find(params[:id])
+    unless current_user.id == item.user_id
+      redirect_to root_path
+    end
   end
 end
