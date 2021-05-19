c02c7c27c1caa3b20c15cb773d8611484d21cb8f
matsunaga1994 <82854197+matsunaga1994@users.noreply.github.com>
2021-05-19 10:45:22 +0900

    コントローラーの修正その２

--- spec/models/purchase_shopping_address_spec.rb
+++ spec/models/purchase_shopping_address_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe PurchaseShoppingAddress, type: :model do
       user = FactoryBot.create(:user)
       item = FactoryBot.create(:item)
       @purchase_shopping_address = FactoryBot.build(:purchase_shopping_address, user_id: user.id, item_id: item.id)
+      sleep(1)
     end
 
     context '内容に問題がない場合' do
